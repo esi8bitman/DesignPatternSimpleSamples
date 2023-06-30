faf82c6aed68d36550239a9835b4a7ea46beac6a
ESI-8Bitman <esi.inc.company@gmail.com>
2023-06-30 16:48:09 +0430

    Added some info to main

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿namespace Practice;
-using Visitor;
+using Visitor; //just use each pattern here
 
 class Program
 {
 
     static void Main(string[] args)
     {
+        //each pattern has a static Runner ... just call it.. no need to instance
         VisitorRunner.Run();
     }
 }
